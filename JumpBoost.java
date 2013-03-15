0a6e3c5c0111cb8bd49b080978ebd36f6bfdb742
Eero911 <eero.syrjo@gmail.com>
2013-03-15 14:18:43 +0200

    Create JumpBoost.java

---
+++ JumpBoost.java
@@ -0,0 +1,56 @@
+package me.eero911.jumpboost;
+ 
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+ 
+public final class JumpBoost extends JavaPlugin implements Listener {
+ 
+    @Override
+    public void onEnable(){
+     getServer().getPluginManager().registerEvents(this, this);
+     getLogger().info("JumpBoost has been enabled!");
+    }
+    @Override
+    public void onDisable(){
+     getLogger().info("JumpBoost has been disabled");
+    }
+    
+    @EventHandler
+    public void enable(PlayerToggleSneakEvent event){
+        Player p = event.getPlayer();
+        
+        if(p.hasPermission("jumpboost.one")) {
+          p.removePotionEffect(PotionEffectType.JUMP);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 0));
+        }
+        if(p.hasPermission("jumpboost.two")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 1));
+        }
+        if(p.hasPermission("jumpboost.three")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 2));
+        }
+    }
+    @EventHandler
+    public void enable(PlayerMoveEvent event){
+        Player p = event.getPlayer();
+        
+        if(p.hasPermission("jumpboost.one")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+        }
+        if(p.hasPermission("jumpboost.two")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+        }
+        if(p.hasPermission("jumpboost.three")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+        }
+    }
+}
